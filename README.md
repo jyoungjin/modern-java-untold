582f662029e7b0fe96745f0b8757b32ea942322f
jyoungjin <ccd1202@naver.com>
2024-03-13 18:15:10 +0900

    feat: 함수형 프로그래밍 function, consumer, predicate, supplier

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # modern-java-untold
+
+---
+
+### 01. 왜 배워야 하나요? 뭐가 좋아요?
+
+- [WhyJava8.java](src%2FWhyJava8.java)
+- [OopAndFpExamples.java](src%2FOopAndFpExamples.java)
+- 간결한 코드로 가독성을 높이고 오류 발생률을 줄일 수 있다.
+
+### 02. `Function`, The Transformer
+
+- [FunctionalInterfaceExamples.java](src%2FFunctionalInterfaceExamples.java)
+- Functional Interface : Interface 중, abstract 메서드를 하나만 가지는 인터페이스 -> 메서드 람다를 사용할 수 있음
+- Identity Function : 변형 없이 입력 값을 그대로 반환하는 함수
+- Function<T, R> : R apply(T t);
+
+### 03. `Consumer`, The Spartan, Give Them Nothing but Take from Them Everything
+
+- [FunctionalInterfaceExamples.java](src%2FFunctionalInterfaceExamples.java)
+- Consumer<T> : void accept(T t);
+
+### 04. `Predicate`, The Judge
+
+- [FunctionalInterfaceExamples.java](src%2FFunctionalInterfaceExamples.java)
+- Predicate<T> : boolean test(T t);
+
+### 05. `Supplier`, The Master of Lazy
+
+- [FunctionalInterfaceExamples.java](src%2FFunctionalInterfaceExamples.java)
+- Supplier<T> : boolean test(T t);
+
+---
+
+출처 :
+https://www.youtube.com/watch?v=mu9XfJofm8U&list=PLRIMoAKN8c6O8_VHOyBOhzBCeN7ShyJ27
---
+++ src/FunctionalInterfaceExamples.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class FunctionalInterfaceExamples {
+
+    public static void main(String[] args) {
+
+        /* Functional Interface : Interface 중, abstract 메서드를 하나만 가지는 인터페이스 -> 메서드 람다를 사용할 수 있음 */
+
+        /* 함수형 인터페이스 : Function */
+        /* 기본 */
+        final Function<String, Integer> toIntBasic = new Function<String, Integer>() {
+            @Override
+            public Integer apply(final String value) {
+                return Integer.parseInt(value);
+            }
+        };
+        /* 람다 */
+        final Function<String, Integer> toIntLambda = (value) -> Integer.parseInt(value);
+        /* 메서드 참조  */
+        final Function<String, Integer> toIntMethodReference = Integer::parseInt;
+
+        final Integer number = toIntMethodReference.apply("100");
+        System.out.println("[Function] number = " + number);
+
+        /* Identity Function : 변형 없이 입력 값을 그대로 반환하는 함수 */
+        final Function<Integer, Integer> identity = Function.identity();
+//        final Function<Integer, Integer> identity = t -> t;
+        System.out.println("[Function] identity = " + identity.apply(999));
+
+        /* 함수형 인터페이스 : Consumer */
+        final Consumer<String> print = s -> System.out.println("[Consumer] " + s);
+        print.accept("consumer method test!!");
+
+        /* 함수형 인터페이스 : Predicate */
+        final Predicate<Integer> isPositive = i -> i > 0;
+        final Predicate<Integer> isLessThan3 = i -> i < 3;
+
+        List<Integer> numbers = Arrays.asList(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5);
+
+        List<Integer> positiveNumbers = filter(numbers, isPositive);
+        List<Integer> lessThanThreeNumbers = filter(numbers, isLessThan3);
+        System.out.println("[Predicate] positiveNumbers = " + positiveNumbers);
+        System.out.println("[Predicate] lessThanThreeNumbers = " + lessThanThreeNumbers);
+
+        /* 함수형 인터페이스 : Predicate */
+        final Supplier<String> helloSupplier = () -> "Hello ";
+        System.out.println("[Supplier] " + helloSupplier.get() + "world");
+
+        long start = System.currentTimeMillis();
+        printIfValidIndex(0, () -> getVeryExpensiveValue());
+        printIfValidIndex(-1, () -> getVeryExpensiveValue());
+        printIfValidIndex(-2, () -> getVeryExpensiveValue());
+        long end = System.currentTimeMillis();
+        System.out.println("It took " + (end - start) / 1000 + " seconds.");
+    }
+
+    private static <T> List<T> filter(List<T> list, Predicate<T> filter) {
+        List<T> result = new ArrayList<>();
+        for (T input : list) {
+            if (filter.test(input)) {
+                result.add(input);
+            }
+        }
+        return result;
+    }
+
+//    private static void printIfValidIndex(int num, String value) {
+//        if (num >= 0) {
+//            System.out.println("[Supplier] value = " + value);
+//        } else {
+//            System.out.println("[Supplier] Invalid");
+//        }
+//    }
+
+    private static void printIfValidIndex(int num, Supplier<String> valueSupplier) {
+        if (num >= 0) {
+            System.out.println("[Supplier] value = " + valueSupplier.get());
+        } else {
+            System.out.println("[Supplier] Invalid");
+        }
+    }
+
+    private static String getVeryExpensiveValue() {
+        try {
+            TimeUnit.SECONDS.sleep(3L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        // Let's just say it has very expensive calculation here!
+        return "Youngjin";
+    }
+}
